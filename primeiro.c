c92c190aed452ded8184f4d4dd1e037a30a05c1a
paulohenriquebiazus <paulo.becker@escola.pr.gov.br>
2025-02-20 12:09:00 -0300

    Add files via upload

---
+++ primeiro.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int calebe;
+    int luiz;
+    int soma;
+    int multiplicacao;
+    luiz = 24;
+    calebe = -54;
+    soma = calebe + luiz;
+    multiplicacao = calebe * luiz;
+    printf("o valor de calebe é %d \n", calebe);
+    printf("o valor de luiz é %d \n", luiz);
+    printf("a soma è %d \n", soma);
+    printf("a multiplicacao é %d", multiplicacao);
+
+    return 0;
+}
